ac14056676cdad1c19e087f1b33a9db1e3f0ac1b
S Abida <abidashaik.c@gmail.com>
2022-07-07 17:34:52 +0530

    initial commit

---
+++ src/brickBreaker/main.java
@@ -0,0 +1,18 @@
+package brickBreaker;
+
+import javax.swing.JFrame;
+
+public class main {
+
+	public static void main(String[] args) {
+		JFrame obj=new JFrame();
+		GamePlay gamePlay=new GamePlay();
+		obj.setBounds(10,10,700,600);
+		obj.setTitle("Brick Breaker");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gamePlay);
+		
+	}
+}
